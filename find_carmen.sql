0727490fff13e80d7da18e0566b6cd3824b1edea
Erick Valencia <evalencia0911@gmail.com>
2024-03-24 10:54:13 -0600

    carmen sandiego - halfway there - hot on her trail

--- find_carmen.sql
+++ find_carmen.sql
@@ -1,12 +1,52 @@
 -- Homework 1
 
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+SELECT name
+FROM country
+WHERE region = 'Southern Europe'
+ORDER BY population ASC
+LIMIT 1;
+
+Output
+Holy See (Vatican City State)
+
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+SELECT code
+FROM country
+WHERE name = "Holy See (Vatican City State)";
+
+Output
+VAT
+
+SELECT lanugage 
+FROM countrylanguage
+WHERE countrycode = 'VAT';
+
+Output
+Italian
 
 -- Clue #3: We have new news on the classes Carmen attended: our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+SELECT countrycode, isofficial
+FROM countrylanguage
+WHERE language = 'Italian';
+
+Output
+ITA
+SMR
+VAT - Already were here
 
 -- Clue #4: We're booking the first flight out: maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+
+SELECT name
+FROM city
+WHERE countrycode = 'SMR'
+
+Output
+Serravalle
+San Marino
+
+Ruling out ITA
 
 -- Clue #5: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
