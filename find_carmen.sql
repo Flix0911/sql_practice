33d1b46d8e2176111f836278de4744e07a1ba8d9
Erick Valencia <evalencia0911@gmail.com>
2024-03-24 11:14:40 -0600

    CARMEN SANDIEGO - I found Carmen Sandiego

--- find_carmen.sql
+++ find_carmen.sql
@@ -50,9 +50,54 @@ Ruling out ITA
 
 -- Clue #5: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll follow right behind you!
 
+SELECT name
+FROM country
+WHERE code = 'SMR';
+
+Output
+Serravalle
+
 -- Clue #6: Oh no, she pulled a switch: there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
+SELECT countrycode, name
+FROM city
+WHERE city.name ILIKE 'Serra';
+
+Output
+BRA
+Serra
+
+SELECT name
+FROM country
+WHERE code = 'BRA';
+
+Output
+Brazil
 
 -- Clue #7: She knows we're on to her: her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock.
 
+-- Reference back to earlier saying the capital
+
+SELECT capital
+FROM country
+WHERE code = 'BRA'
+
+
+Output
+211
+
+SELECT name
+from city
+WHERE id = 211
+
+Output
+Bras�lia
 -- Lucky for us, she's getting cocky. She left us a note, and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
+91804 - current population
+9185 - when she get there
+
+SELECT name, countrycode, district
+from city
+WHERE population = 91084
+
+She is in Santa Monica, CA, USA
