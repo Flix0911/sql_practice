910f29a27837f9f31f4814ce3f2591d852a3b074
Erick Valencia <evalencia0911@gmail.com>
2024-03-24 09:32:12 -0600

    NFL queries 1

--- nfl.sql
+++ nfl.sql
@@ -1,7 +1,36 @@
+-- Create below table for Teams
+create table teams(
+	id serial primary key,
+	name varchar(255) not null,
+	stadium varchar(255),
+	division varchar(255),
+	conference varchar(255),
+	head_coach varchar(255),
+	active boolean
+);
+
+-- Create below table for Players
+create table players(
+	id serial primary key,
+	name varchar(255) not null,
+	position varchar(255),
+	salary integer,
+	team_id integer references teams
+);
+
+
 -- List the names of all NFL teams
+SELECT name FROM teams;
+
 -- List the stadium name and head coach of all NFC teams
+SELECT stadium, head_coach FROM teams;
+
 -- List the head coaches of the AFC South
+SELECT head_coach FROM teams WHERE conference = 'AFC' AND division = 'South'
+
 -- The total number of players in the NFL
+SELECT COUNT(*) FROM players;
+
 -- The team names and head coaches of the NFC North and AFC East
 -- The 50 players with the highest salaries
 -- The average salary of all NFL players
